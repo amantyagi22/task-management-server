415eed710bfae3ccbbbd576f6637147c5e331a90
Aman Tyagi <aman@playo.co>
2025-03-03 01:17:23 +0530

    update the prisma changes

--- src/clients/db/index.ts
+++ src/clients/db/index.ts
@@ -2,6 +2,11 @@ import { PrismaClient } from '@prisma/client';
 
 const prismaClient = new PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL
+        }
+    },
 });
 
 export default prismaClient;
