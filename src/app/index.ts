059bd94fb0334f30395219b439f844a8318ba19c
Aman Tyagi <aman@playo.co>
2025-03-03 20:58:03 +0530

    update the cors error

--- src/app/index.ts
+++ src/app/index.ts
@@ -11,6 +11,7 @@ export async function initServer() {
     app.use(json());
     app.use(cors({
         origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }));
     
